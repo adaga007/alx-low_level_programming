355004f7735b6a295555d1ab30534313504a4090
adaga007 <happyadaga@gmail.com>
2023-03-17 16:23:44 -0700

    9-print_comb.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -11,9 +11,9 @@ for (i = 0; i <= 8; i++)
 {
 for (j = i + 1; j <= 9; j++)
 {
-putchar(i);
-putchar(j);
-if (i == 8 && j == 9)
+putchar(i + '0');
+putchar(j + '0');
+if ((i == 8) && (j == 9))
 {
 continue;
 }
