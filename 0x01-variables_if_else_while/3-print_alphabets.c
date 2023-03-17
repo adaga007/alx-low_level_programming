f513ce269c6814834b2fc26098073dd0f20034a4
adaga007 <happyadaga@gmail.com>
2023-03-17 11:38:10 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - a programm
+ * Return: 0 (success)
+ */
+int main(void)
+{
+int a = 97;
+int b = 65;
+while (a <= 122)
+{
+putchar(a);
+a++
+}
+while (b <= 90)
+{
+putchar(b);
+b++
+}
+putchar('\n');
+return (0);
+}
