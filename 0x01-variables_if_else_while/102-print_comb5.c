ff80bdb97996889a59f08724d6e8a6be35aa2d65
adaga007 <happyadaga@gmail.com>
2023-03-19 12:43:02 -0700

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,49 +1,32 @@
 #include <stdio.h>
 /**
- * main - pr
- * Return: 0 (sucess)
+ * main - entry point
+ *
+ * Return: alway returns 0
+ *
  */
 int main(void)
 {
-int i;
-int j;
-int k;
-int l;
-for (i = 0; i <= 9; i++)
-{
-for (j = 0; j <= 8; j++)
-{
-for (k = 0; k <= 9; k++)
-{
-for (l = 0; l <= 9; l++)
-{
-if (i == k && j == l)
-{
-continue;
-}
-if (i == l && j == k)
-{
-continue;
-}
-if (i == j && i == k && i == l)
-{
-continue;
-}
-putchar(i + '0');
-putchar(j + '0');
-putchar(' ');
-putchar(k + '0');
-putchar(l + '0');
-if ((i == 9 && j == 8) && (k == 9 && l == 9))
-{
-continue;
-}
-putchar(',');
-putchar(' ');
-}
-}
-}
-}
-putchar('\n');
-return (0);
+	int i;
+	int j;
+
+	for (i = 0; i <= 99; i++)
+	{
+		for (j = i + 1; j <= 99; j++)
+		{
+			putchar(i / 10 + '0');
+			putchar(i % 10 + '0');
+			putchar(' ');
+			putchar(j / 10 + '0');
+			putchar(j % 10 + '0');
+
+			if (!(i == 98 && j == 99))
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
 }
