a939969fb00bfb7f56dc662808c8bc8c7ed29e1e
adaga007 <happyadaga@gmail.com>
2023-04-08 04:06:20 -0700

    1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <stdio.h>
+#define UNUSED(x) (void)(x)
 /**
  * main - a program that prints the number of arguments passed into it.
  * @argc: argument count
@@ -8,5 +9,6 @@
  */
 int main(int argc, char *argv[])
 {
+UNUSED(*argv);
 printf("%d\n", argc);
 }
