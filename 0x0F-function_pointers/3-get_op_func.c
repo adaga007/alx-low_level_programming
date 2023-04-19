baf3ea6df3fd47321013a655ad3f7d36e712f5ad
adaga007 <happyadaga@gmail.com>
2023-04-19 13:50:16 -0700

    3-get_op_func.c

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -6,26 +6,25 @@
  * @s: function
  * Return: int value
  */
-int (*get_op_func(char *s))(int, int)
-{
-	op_t ops[] = {
-		{"+", op_add},
-		{"-", op_sub},
-		{"*", op_mul},
-		{"/", op_div},
-		{"%", op_mod},
-		{NULL, NULL}
-	};
-	int index;
-
-	index = 0;
-	while (ops[index].f != NULL)
+	int (*get_op_func(char *s))(int, int)
 	{
-		if (*s == *(ops[index].op) && s[1] == '\0')
-			return (ops[index].f);
-		index++;
+		op_t ops[] = {
+			{"+", op_add},
+			{"-", op_sub},
+			{"*", op_mul},
+			{"/", op_div},
+			{"%", op_mod},
+			{NULL, NULL},
+		};
+
+
+		int i = 0;
+
+
+		while (ops[i].op != NULL && *(ops[i].op) != *s)
+			i++;
+
+
+		return (ops[i].f);
 	}
 
-	printf("Error\n");
-	exit(99);
-}
