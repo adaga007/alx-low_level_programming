70afd1e9e8d63c1269d759b84cccc8e69eb6ac6d
adaga007 <happyadaga@gmail.com>
2023-04-19 13:02:55 -0700

    1-array_iterator.c

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "3-calc.h"
+/**
+ * main -  a program that performs simple operations.
+ * @argc: arguments
+ * @argv: array of args
+ * Return: result
+ */
+int main(int argc, char *argv[])
+{
+	if (argc == 4)
+	{
+		int a;
+		int b;
+		int (*func_ptr)(int, int);
+
+		a = atoi(argv[1]);
+		b = atoi(argv[3]);
+		func_ptr = get_op_func(argv[2]);
+		if ((*argv[2] == '%' || *argv[2] == '/') && b == 0)
+		{
+			printf("Error\n");
+			exit(100);
+		}
+		printf("%d\n", func_ptr(a, b));
+		return (0);
+	}
+	printf("Error\n");
+	exit(98);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,53 @@
+#include <stdlib.h>
+#include "3-calc.h"
+/**
+ * op_add - function to add tow int numbers
+ * @a: first number
+ * @b: second number
+ * Return: sum
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * op_sub - function to get difference of two int numbers
+ * @a: first number
+ * @b: second number
+ * Return: difference
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul - function to multiply two in numbers
+ * @a: first number
+ * @b: second number
+ * Return: multipliy
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div - function to divide two numbers
+ * @a: first number
+ * @b: second number
+ * Return: division
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * op_mod - function to find modulos of two numbers
+ * @a: first number
+ * @b: second number
+ * Return: modulo of ints
+ */
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
