64469327ad3975025f2a3426044ab8f5b7135914
adaga007 <happyadaga@gmail.com>
2023-04-19 02:02:10 -0700

    0-print_name.c

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+#include "function_pointers.h"
+/**
+ * print_name - a function that prints a name
+ * @name: name
+ * @f: function
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name != NULL && f != NULL)
+		f(name);
+}
