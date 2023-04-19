cf19f7fab3e48b04abd90c150f90bd6204a5976a
adaga007 <happyadaga@gmail.com>
2023-04-19 13:52:13 -0700

    3-main.c

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -7,26 +7,43 @@
  * @argv: array of args
  * Return: result
  */
-int main(int argc, char *argv[])
-{
-	if (argc == 4)
+	int main(int __attribute__((__unused__)) argc, char *argv[])
 	{
-		int a;
-		int b;
-		int (*func_ptr)(int, int);
+		int num1, num2;
+		char *op;
 
-		a = atoi(argv[1]);
-		b = atoi(argv[3]);
-		func_ptr = get_op_func(argv[2]);
-		if ((*argv[2] == '%' || *argv[2] == '/') && b == 0)
+
+		if (argc != 4)
+		{
+			printf("Error\n");
+			exit(98);
+		}
+
+
+		num1 = atoi(argv[1]);
+		op = argv[2];
+		num2 = atoi(argv[3]);
+
+
+		if (get_op_func(op) == NULL || op[1] != '\0')
+		{
+			printf("Error\n");
+			exit(99);
+		}
+
+
+		if ((*op == '/' && num2 == 0) ||
+		    (*op == '%' && num2 == 0))
 		{
 			printf("Error\n");
 			exit(100);
 		}
-		printf("%d\n", func_ptr(a, b));
+
+
+		printf("%d\n", get_op_func(op)(num1, num2));
+
+
 		return (0);
 	}
-	printf("Error\n");
-	exit(98);
-}
+
 
