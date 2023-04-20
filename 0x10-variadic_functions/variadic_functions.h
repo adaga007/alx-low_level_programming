bcb1d93a2489250910b1081f1cb1b26da26b4e5f
adaga007 <happyadaga@gmail.com>
2023-04-20 08:38:12 -0700

    3-print_all.c

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,22 +1,21 @@
-#ifndef VARIADIC_FUNCTIONS_H
-#define VARIADIC_FUNCTIONS_H
+#ifndef _VARIADIC_FUNCTIONS_H_
+#define _VARIADIC_FUNCTIONS_H_
 #include <stdarg.h>
+#include <stdio.h>
+/*
+ * struct check - paramenters to check against
+ * @c: type
+ * @f: the function to send it to
+ */
+typedef struct check
+{
+	char *chk;
+	void (*f)(va_list list);
+} printer;
+
+int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
-int print_c(va_list a);
-int print_i(va_list a);
-int print_f(va_list a);
-int print_s(va_list a);
-/**
- * struct op - structure
- * @c: character
- * @f: function
- */
-typedef struct op
-{
-	char *c;
-	int (*f)(va_list a);
-} printer;
 #endif
