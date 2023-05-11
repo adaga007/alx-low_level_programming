83fef121ca907dd792961b8219ac86ba7b055016
adaga007 <happyadaga@gmail.com>
2023-05-11 08:11:52 -0700

    3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -11,27 +11,27 @@ int main(int ac, char *av[])
 	int fsrc, fdest, rd, clf, clt;
 	char buffer[1024];
 
-	if (argc != 3)
+	if (ac != 3)
 	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-	fsrc = open(argv[1], O_RDONLY);
+	fsrc = open(av[1], O_RDONLY);
 	if (fsrc  == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
 		exit(98);
 	}
-	fdest = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	fdest = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	while ((rd = read(fsrc, buffer, 1024)) > 0)
 		if (fdest == -1 || (write(fdest, buffer, rd) != rd))
 		{
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
 			exit(99);
 		}
 	if (rd == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
 		exit(98);
 	}
 	clf = close(fsrc);
